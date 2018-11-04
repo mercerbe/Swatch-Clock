1459ac734539671148946854c03ad59f1c0e4ed8
Benjamin Mercer <benjamin.mercer@Administrators-MacBook-Pro-2.local>
2018-11-04 18:45:55 -0500

    update vs setting, prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,66 +1,63 @@
-import React, { Component } from 'react';
-import {Blob} from "react-blob";
-import './App.css';
+import React, { Component } from "react";
+import { Blob } from "react-blob";
+import "./App.css";
 
-//init date and time variables  
+//init date and time variables//
 let d, h, m, s, color, blobStyle;
 
-//function to display time 
+//function to display time
 const displayTime = () => {
-d = new Date();
-h = d.getHours();
-m = d.getMinutes();
-s = d.getSeconds();
+	d = new Date();
+	h = d.getHours();
+	m = d.getMinutes();
+	s = d.getSeconds();
 
-//add 0 to hms less than 10
-if(h < 10) {
-  h = `0${h}`
-}
-if(m < 10) {
-  m = `0${m}`
-}
-if(s < 10) {
-  s = `0${s}`
-}
+	//add 0 to hms less than 10
+	if (h < 10) {
+		h = `0${h}`;
+	}
+	if (m < 10) {
+		m = `0${m}`;
+	}
+	if (s < 10) {
+		s = `0${s}`;
+	}
 
-//set color format 
-color = `#${h}${m}${s}`
+	//set color format
+	color = `#${h}${m}${s}`;
 
-//styles 
-blobStyle = {
-  color: 'white',
-  fontSize: '175px',
-  backgroundColor: color
-}
-
-}
-
+	//styles
+	blobStyle = {
+		color: "white",
+		fontSize: "175px",
+		backgroundColor: color
+	};
+};
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { date: color };
+	}
 
-  constructor(props) {
-    super(props);
-    this.state = {date: color};
-  }
+	componentDidMount() {
+		setInterval(displayTime(), 1000);
+		this.setState({
+			date: color
+		});
 
-  componentDidMount() {
-    setInterval(displayTime(), 1000)
-    this.setState({
-      date: color
-    })
+		console.log(this.state.date);
+	}
 
-    console.log(this.state.date)
-  }
-
-  render() {
-    return (
-      <div className="App">
-      <Blob style={blobStyle} size="5em" textAlign="center">
-        <span>{this.state.date}</span>
-      </Blob> 
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Blob style={blobStyle} size="5em" textAlign="center">
+					<span>{this.state.date}</span>
+				</Blob>
+			</div>
+		);
+	}
 }
 
 export default App;
