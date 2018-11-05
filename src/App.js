140cc7e98b87cf6fd279623ea7d62058014acb98
Benjamin Mercer <benjamin.mercer@Administrators-MacBook-Pro-2.local>
2018-11-04 20:57:24 -0500

    clock updated, media queries

--- src/App.js
+++ src/App.js
@@ -2,58 +2,47 @@ import React, { Component } from "react";
 import { Blob } from "react-blob";
 import "./App.css";
 
-//init date and time variables//
-let d, h, m, s, color, blobStyle;
-
-//function to display time
-const displayTime = () => {
-	d = new Date();
-	h = d.getHours();
-	m = d.getMinutes();
-	s = d.getSeconds();
-
-	//add 0 to hms less than 10
-	if (h < 10) {
-		h = `0${h}`;
-	}
-	if (m < 10) {
-		m = `0${m}`;
-	}
-	if (s < 10) {
-		s = `0${s}`;
-	}
-
-	//set color format
-	color = `#${h}${m}${s}`;
-
-	//styles
-	blobStyle = {
-		color: "white",
-		fontSize: "175px",
-		backgroundColor: color
-	};
-};
-
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { date: color };
+
+		this.state = {
+			time: new Date()
+		};
 	}
 
 	componentDidMount() {
-		setInterval(displayTime(), 1000);
-		this.setState({
-			date: color
-		});
-
-		console.log(this.state.date);
+		setInterval(this.update, 1000);
 	}
 
+	update = () => {
+		this.setState({
+			time: new Date()
+		});
+	};
+
 	render() {
+		let h = this.state.time.getHours();
+		let m = this.state.time.getMinutes();
+		let s = this.state.time.getSeconds();
+		//add 0 to hms less than 10
+		if (h < 10) {
+			h = `0${h}`;
+		}
+		if (m < 10) {
+			m = `0${m}`;
+		}
+		if (s < 10) {
+			s = `0${s}`;
+		}
+
+		//set color format
+		const color = `#${h}${m}${s}`;
+
 		return (
 			<div className="App">
-				<Blob style={blobStyle} size="5em" textAlign="center">
-					<span>{this.state.date}</span>
+				<Blob className="blob" style={{color: "white", backgroundColor: color}} size="5em" textAlign="center">
+					<span>{color}</span>
 				</Blob>
 			</div>
 		);
